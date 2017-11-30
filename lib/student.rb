e0cebfc41170f44c1d4a9637ebb624a51861e43f
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-30 22:32:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.column_names
-    
+  self.column_names.each do |col|
+    attr_accessor col.to_s
   end
 end
