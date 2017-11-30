37bbb818b60ed98a12f5fa3db9b4cc8ca8bfd9a0
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-30 22:55:21 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,7 @@ class InteractiveRecord
 
   def self.find_by(attr)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attr.keys[0].to_s} == ?"
+    DB[:conn].execute(sql, attr.values)
   end
 
 
