297c1a5d06a1fdf9063d84c8d4fd1614c57f674a
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-30 22:36:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,4 +5,9 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+  end
 end
